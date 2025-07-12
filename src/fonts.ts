ebbc71fc7ece04a55be317e3f7475239b8745f8f
tobicode <tobijoshua919@gmail.com>
2025-07-11 22:47:39 -0400

    Added neccessary icons and fonts

---
+++ src/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local'
+
+export const inter = localFont({
+    src: "/assets/fonts/Inter_18pt-Black.ttf",
+    display: "swap",
+    variable: "--font-inter",
+  });
