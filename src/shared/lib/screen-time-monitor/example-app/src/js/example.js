b12eb0c7ee38a3a3fc0802292411512e0332829d
Ife-adewunmi <ifeoluwa.adewunmi94@gmail.com>
2025-08-29 11:50:33 +0100

    fix build error

--- src/shared/lib/screen-time-monitor/example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { ScreenTimeMonitor } from 'screen-time-monitor';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    ScreenTimeMonitor.echo({ value: inputValue })
-}
